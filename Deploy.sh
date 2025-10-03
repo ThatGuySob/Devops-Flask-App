a801cae1e1f5d535e961ac33e0c01315ba8128ef
Bartosz Sobiegraj <101882926+ThatGuySob@users.noreply.github.com>
2025-10-03 13:16:38 +0100

    Update Deploy.sh

--- Deploy.sh
+++ Deploy.sh
@@ -9,3 +9,33 @@ python -m venv .my_venv
 source .my_venv/bin/activate
 
 pip install flask
+
+cat <<EOL > hello.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def say_hello():
+    return '<p>Hello World! I am a Flask app!</p>' \
+           '<a href="/about">About Page</a>' \
+           '<a href="/contact">Contact Page</a>'
+
+@app.route('/about')
+def about_page():
+    return '<p>Hello, World, I am the about page! <a href="https://python.org">Python!</a></p>' \
+           '<a href="/">Home Page</a>' \
+           '<a href="/contact">Contact Page</a>'
+
+@app.route('/contact')
+def contact_page():
+    return '<p>Hello, World c23398751@mytudublin.ie</p>' \
+           '<a href="/">Home Page</a>' \
+           '<a href="/about">About Page</a>'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
+EOL
+
+# Run the Flask app
+python hello.py
